1fe98436ef746980b137f05d27322e23b544be2e
Debbie Bourne <dblashrink@hotmail.com>
2016-09-12 15:40:36 -0500

    basic shell of app

---
+++ app/app.js
@@ -0,0 +1,5 @@
+// apiKey: "AIzaSyDA6Kvb42gybEpW4JqK2JRi9DPWzDkqfZw"
+// databaseURL: https://flower-power-angular.firebaseio.com
+"use strict";
+
+console.log("app.js is on deck");
---
+++ app/controllers/ListFlowersCtrl.js
@@ -0,0 +1,5 @@
+"use strict";
+
+app.controller("ListFlowersCtrl", function ($scope, FlowerFactory, $routeParams, $location) {
+
+});
